97a07cf36aaee8d65f28514ead775c8fa4ee9351
everamzah <everamzah@gmail.com>
2015-08-04 21:08:05 -0400

    Use w3c colors for xpanes_colors

--- mods/xpanes_colors/init.lua
+++ mods/xpanes_colors/init.lua
@@ -1,26 +1,26 @@
 local xpanes_colors = {}
 
 xpanes_colors.dyes = {
-	{"grey",	"Grey",		"#9C9C9C"},
-	{"dark_grey",	"Dark Grey",	"#494949"},
-	{"black",	"Black",	"#292929"},
-	{"violet",	"Violet",	"#480680"},
-	{"blue",	"Blue",		"#00519D"},
-	{"cyan",	"Cyan",		"#00959D"},
-	{"dark_green",	"Dark Green", 	"#2B7B00"},
-	{"green",	"Green",	"#67EB1C"},
-	{"yellow",	"Yellow",	"#FCF611"},
-	{"brown",	"Brown",	"#6C3800"},
-	{"orange",	"Orange",	"#E0601A"},
-	{"red",		"Red",		"#C91818"},
-	{"magenta",	"Magenta",	"#D80481"},
-	{"pink",	"Pink",		"#FFA5A5"},
+	{"grey",	"Grey",		"#9C9C9C",	"gray"},
+	{"dark_grey",	"Dark Grey",	"#494949",	"darkgray"},
+	{"black",	"Black",	"#292929", 	"black"},
+	{"violet",	"Violet",	"#480680",	"violet"},
+	{"blue",	"Blue",		"#00519D",	"blue"},
+	{"cyan",	"Cyan",		"#00959D",	"cyan"},
+	{"dark_green",	"Dark Green", 	"#2B7B00",	"darkgreen"},
+	{"green",	"Green",	"#67EB1C",	"green"},
+	{"yellow",	"Yellow",	"#FCF611",	"yellow"},
+	{"brown",	"Brown",	"#6C3800",	"brown"},
+	{"orange",	"Orange",	"#E0601A",	"orange"},
+	{"red",		"Red",		"#C91818",	"red"},
+	{"magenta",	"Magenta",	"#D80481",	"magenta"},
+	{"pink",	"Pink",		"#FFA5A5",	"pink"},
 }
 
 for i, v in ipairs(xpanes_colors.dyes) do
 	local name = v[1]
 	local desc = v[2]
-	local hex = v[3]
+	local hex = v[4] --[3], now w3c color
 	xpanes.register_pane("pane_"..name, {
 		description = "Glass Pane ("..desc..")",
 		tiles = {"xpanes_space.png"},
