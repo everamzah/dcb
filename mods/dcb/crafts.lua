035e6f0643514184ffdd1d0efd8cc4445b94953e
everamzah <everamzah@gmail.com>
2015-08-06 06:24:25 -0400

    Todo - redo crafting guide.

--- mods/dcb/crafts.lua
+++ mods/dcb/crafts.lua
@@ -1,8 +1,25 @@
-for name, item in pairs(minetest.registered_items) do
-	if (name and name ~= "") then
-		local recipe = minetest.get_all_craft_recipes(name)
-		if recipe ~= nil then
-			--print(dump(recipe))
+-- TODO Make sign_wall and lcd_pc craft_guide nodes open Zeg9's ZCG
+
+--[[
+
+minetest.after(5, function()
+	local sort = {}
+	local search = minetest.registered_items
+	for _, v in pairs(search) do
+		if minetest.get_item_group(_, "not_in_creative_inventory") == 0 then
+			if minetest.get_craft_recipe(_).width ~= 0 then
+				table.insert(sort, _)
+			end
 		end
 	end
-end
+	table.sort(sort)
+	--print(dump(sort))
+	local recipes = {}
+	for x, y in pairs(sort) do
+		local recipe = minetest.get_craft_recipe(y)
+		table.insert(recipes, recipe.items)
+		--print(dump(recipe.items))
+	end
+end)
+
+--]
