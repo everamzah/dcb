e0f27ef19e3f362652e0442cc9b3cf1a04a5eab4
everamzah <everamzah@gmail.com>
2015-08-11 10:45:34 -0400

    Revert serveressentials, add memorandum in a stripped state

--- mods/serveressentials/settings.lua
+++ mods/serveressentials/settings.lua
@@ -1,27 +1,19 @@
---AFK_CHECK = true --Whether or not to automatically kick afk players
-AFK_CHECK = minetest.setting_get("afk_check") or true
---MAX_AFK_TIME = 600 --Max time allowed afk before kick 
-MAX_AFK_TIME = minetest.setting_get("max_afk_time") or 600
-AFK_CHECK_INTERVAL = minetest.setting_get("afk_check_interval") or 1
-AFK_WARN_TIME = minetest.setting_get("afk_warn_time") or 20
+AFK_CHECK = true --Whether or not to automatically kick afk players
+MAX_AFK_TIME = 300 --Max time allowed afk before kick 
+AFK_CHECK_INTERVAL = 1 --Number of seconds between activity checks
+AFK_WARN_TIME = 20 --Number of seconds before being kicked that a player will start to be warned
 
-SHOW_FIRST_TIME_JOIN_MSG = minetest.setting_get("show_first_time_join_msg") or true
+SHOW_FIRST_TIME_JOIN_MSG = true --Whether or not to show FIRST_TIME_JOIN_MSG if a new player joins
+FIRST_TIME_JOIN_MSG = " has joined the server for the first time, Welcome!" --Message to broadcast to all players when a new player joins the server, will follow the players name
 
-local name = minetest.setting_get("server_name") or "[SERVER]"
-FIRST_TIME_JOIN_MSG = " has joined the server for the first time!  Welcome to "..name
+BROADCAST_PREFIX = "[SERVER]" --All messages sent with the /broadcast command will be prefixed with this
 
-BROADCAST_PREFIX = minetest.setting_get("broadcast_prefix") or "[SERVER: "..name.."]" or "[SERVER]"
-
-REMOVE_BONES = minetest.setting_get("remove_bones") or false
-REMOVE_BONES_TIME = minetest.setting_get("remove_bones_time") or 600
-
-KICK_CHATSPAM = minetest.setting_get("kick_chatspam") or true
-MAX_CHAT_MSG_LENGTH = minetest.setting_get("max_chat_msg_length") or 400
-
-
-
-
-DISALLOWED_NODES = {
+DISALLOWED_NODES = { --These nodes will be immediatly removed if they are placed. Players with the disallowednodes priv can place them
 	"tnt:tnt",
 }
 
+REMOVE_BONES = true --If true, remove bones after REMOVE_BONES_TIME seconds
+REMOVE_BONES_TIME = 1800 --Remove bones after this amount of time (seconds)
+
+KICK_CHATSPAM = true --If true, players who send a chat message longer than MAX_CHAT_MSG_LENGTH will be kicked
+MAX_CHAT_MSG_LENGTH = 400 
