1462eacf726156e6b55c81fa63877344d6ade8a3
everamzah <everamzah@gmail.com>
2015-08-22 15:21:52 -0400

    Slim

--- mods/serveressentials/settings.lua
+++ mods/serveressentials/settings.lua
@@ -3,7 +3,7 @@ MAX_AFK_TIME = 300 --Max time allowed afk before kick
 AFK_CHECK_INTERVAL = 1 --Number of seconds between activity checks
 AFK_WARN_TIME = 20 --Number of seconds before being kicked that a player will start to be warned
 
-SHOW_FIRST_TIME_JOIN_MSG = true --Whether or not to show FIRST_TIME_JOIN_MSG if a new player joins
+SHOW_FIRST_TIME_JOIN_MSG = false --Whether or not to show FIRST_TIME_JOIN_MSG if a new player joins
 FIRST_TIME_JOIN_MSG = " has joined the server for the first time, Welcome!" --Message to broadcast to all players when a new player joins the server, will follow the players name
 
 BROADCAST_PREFIX = "[SERVER]" --All messages sent with the /broadcast command will be prefixed with this
@@ -12,8 +12,8 @@ DISALLOWED_NODES = { --These nodes will be immediatly removed if they are placed
 	"tnt:tnt",
 }
 
-REMOVE_BONES = true --If true, remove bones after REMOVE_BONES_TIME seconds
+REMOVE_BONES = false --If true, remove bones after REMOVE_BONES_TIME seconds
 REMOVE_BONES_TIME = 1800 --Remove bones after this amount of time (seconds)
 
-KICK_CHATSPAM = true --If true, players who send a chat message longer than MAX_CHAT_MSG_LENGTH will be kicked
+KICK_CHATSPAM = false --If true, players who send a chat message longer than MAX_CHAT_MSG_LENGTH will be kicked
 MAX_CHAT_MSG_LENGTH = 400 
