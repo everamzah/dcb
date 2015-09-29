005af3e00b8a41d00d9f0ecfdff796c8b9f8f747
everamzah <everamzah@gmail.com>
2015-09-29 04:42:07 -0400

    Take dcb tools out of creative inventory

--- mods/dcb/init.lua
+++ mods/dcb/init.lua
@@ -1,71 +1,6 @@
 dcb = {}
 
-local sstep = 0.3 --tonumber(minetest.setting_get("dedicated_server_step"))
-
-minetest.register_craftitem("dcb:tool", {
-	description = "Powerful multitool",
-	inventory_image = "default_tool_woodpick.png^default_obsidian_shard.png",
-	stack_max = 1,
-	range = 7.0,
-	liquids_pointable = false,
-	tool_capabilities = {
-		groupcaps = {
-			snappy={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
-			choppy={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
-			cracky={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
-			crumbly={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
-			oddly_breakable_by_hand={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
-		},
-		damage_groups = {fleshy=10},
-	},
-	after_use = function(itemstack, user, node, digparams)
-		local player = user:get_player_name()
-		if not minetest.check_player_privs(player, {ban=true}) then
-			minetest.kick_player(player, "You may not use this tool.")
-			minetest.log("action", player.." used the DCB multitool without the ban priv.")
-			return "default:paper" -- yellow card
-		else
-			return itemstack
-		end
-	end,
-})
-
-minetest.register_craftitem("dcb:dcb", {
-	description = "DCB Tool removes doors, chests, and bones",
-	inventory_image = "default_obsidian_shard.png",
-	stack_max = 1,
-	liquids_pointable = false,
-	on_use = function(itemstack, user, pointed_thing)
-		local pos = minetest.get_pointed_thing_position(pointed_thing)
-		if not minetest.check_player_privs(user:get_player_name(), {delprotect=true}) then
-			minetest.log("action", user:get_player_name().." tried to use the DCB without delprotect priv.")
-			return
-		end
-		if not pos then
-			return
-		else
-			local node = minetest.get_node(pos)
-			if node["name"] == "default:chest_locked" then --convert to regular chest
-				--[[
-				local meta = minetest.get_meta(pos)
-				local inv = meta:get_inventory()
-				local size = inv:get_size("main")
-				for i in size do
-					--shop.spew(pos, "default:chest_locked")
-					print(i)
-				end
-				--]]
-				minetest.remove_node(pos)
-			elseif string.match(node["name"], "doors:door_steel") then --pop steel door on ground
-				minetest.remove_node(pos)
-			elseif node["name"] == "bones:bones" then --pop items around bones, including bones:bones item entity
-				minetest.remove_node(pos)
-			else
-				minetest.remove_node(pos)
-			end
-		end
-	end,
-})
+local modpath = minetest.get_modpath("dcb")
 
 if minetest.setting_getbool("print_fields") then
 	minetest.register_on_player_receive_fields(function(player, formname, fields)
@@ -73,13 +8,14 @@ if minetest.setting_getbool("print_fields") then
 	end)
 end
 
-dofile(minetest.get_modpath("dcb").."/overrides.lua")
-dofile(minetest.get_modpath("dcb").."/book_reader.lua")
---dofile(minetest.get_modpath("dcb").."/post_office.lua")
-dofile(minetest.get_modpath("dcb").."/give_initial_stuff.lua")
-dofile(minetest.get_modpath("dcb").."/creative.lua")
-dofile(minetest.get_modpath("dcb").."/nopvp.lua")
-dofile(minetest.get_modpath("dcb").."/legacy_replacer.lua")
-dofile(minetest.get_modpath("dcb").."/crafting.lua") -- Fence post
-dofile(minetest.get_modpath("dcb").."/sethome.lua")
-dofile(minetest.get_modpath("dcb").."/serveressentials.lua")
+dofile(modpath.."/tools.lua")
+dofile(modpath.."/overrides.lua")
+dofile(modpath.."/book_reader.lua")		-- Send to cbd
+--dofile(modpath.."/post_office.lua")		-- Send to cbd
+dofile(modpath.."/give_initial_stuff.lua")
+dofile(modpath.."/creative.lua")
+dofile(modpath.."/nopvp.lua")
+dofile(modpath.."/legacy_replacer.lua")
+dofile(modpath.."/crafting.lua")		-- Fence post, send to cbd?
+dofile(modpath.."/sethome.lua")
+dofile(modpath.."/serveressentials.lua")
---
+++ mods/dcb/tools.lua
@@ -0,0 +1,68 @@
+local sstep = 0.3 --tonumber(minetest.setting_get("dedicated_server_step"))
+
+minetest.register_craftitem("dcb:tool", {
+	description = "Powerful multitool",
+	inventory_image = "default_tool_woodpick.png^default_obsidian_shard.png",
+	stack_max = 1,
+	range = 7.0,
+	liquids_pointable = false,
+	groups = {not_in_creative_inventory=1},
+	tool_capabilities = {
+		groupcaps = {
+			snappy={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
+			choppy={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
+			cracky={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
+			crumbly={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
+			oddly_breakable_by_hand={times={[1]=sstep, [2]=sstep, [3]=sstep}, maxwear=0, maxlevel=3},
+		},
+		damage_groups = {fleshy=10},
+	},
+	after_use = function(itemstack, user, node, digparams)
+		local player = user:get_player_name()
+		if not minetest.check_player_privs(player, {ban=true}) then
+			minetest.kick_player(player, "You may not use this tool.")
+			minetest.log("action", player.." used the DCB multitool without the ban priv.")
+			return "default:paper" -- yellow card
+		else
+			return itemstack
+		end
+	end,
+})
+
+minetest.register_craftitem("dcb:dcb", {
+	description = "DCB Tool removes doors, chests, and bones",
+	inventory_image = "default_obsidian_shard.png",
+	stack_max = 1,
+	liquids_pointable = false,
+	groups = {not_in_creative_inventory=1},
+	on_use = function(itemstack, user, pointed_thing)
+		local pos = minetest.get_pointed_thing_position(pointed_thing)
+		if not minetest.check_player_privs(user:get_player_name(), {delprotect=true}) then
+			minetest.log("action", user:get_player_name().." tried to use the DCB without delprotect priv.")
+			return
+		end
+		if not pos then
+			return
+		else
+			local node = minetest.get_node(pos)
+			if node["name"] == "default:chest_locked" then --convert to regular chest
+				--[[
+				local meta = minetest.get_meta(pos)
+				local inv = meta:get_inventory()
+				local size = inv:get_size("main")
+				for i in size do
+					--shop.spew(pos, "default:chest_locked")
+					print(i)
+				end
+				--]]
+				minetest.remove_node(pos)
+			elseif string.match(node["name"], "doors:door_steel") then --pop steel door on ground
+				minetest.remove_node(pos)
+			elseif node["name"] == "bones:bones" then --pop items around bones, including bones:bones item entity
+				minetest.remove_node(pos)
+			else
+				minetest.remove_node(pos)
+			end
+		end
+	end,
+})
