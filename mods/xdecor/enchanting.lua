8ece9539640d51162dca864515f2abec6c764c1d
James Stevenson <everamzah@gmail.com>
2016-02-13 10:52:24 -0500

    Try aliasing old enchanted tools

--- mods/xdecor/enchanting.lua
+++ mods/xdecor/enchanting.lua
@@ -188,7 +188,7 @@ for enchant in tooldef[2]:gmatch("[%w_]+") do
 					full_punch_interval = full_punch_interval, max_drop_level = max_drop_level
 				}
 			})
-			minetest.register_alias("xdecor:enchanted_"..tool.."_"..material.."_"..enchant, ":"..mod..":enchanted_"..tool.."_"..material.."_"..enchant)
+			minetest.register_alias("xdecor:enchanted_"..tool.."_"..material.."_"..enchant, mod..":enchanted_"..tool.."_"..material.."_"..enchant)
 		end
 
 		if mod == "3d_armor" then
