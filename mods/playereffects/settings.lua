176658ce0e8a214369bfed7042197a288e695f45
James Stevenson <everamzah@gmail.com>
2016-02-15 07:33:29 -0500

    Make playereffects autosaving optional

--- mods/playereffects/settings.lua
+++ mods/playereffects/settings.lua
@@ -6,10 +6,10 @@
 playereffects.use_hud = true
 
 -- Wheather to use autosave (true or false)
-playereffects.use_autosave = true
+playereffects.use_autosave = minetest.is_yes(minetest.setting_get("playereffects_autosave")) --true
 
 -- The time interval between autosaves, in seconds (only used when use_autosave is true)
-playereffects.autosave_time = 20
+playereffects.autosave_time = tonumber(minetest.setting_get("playereffects_autosave")) or 20
 
 -- If true, this loads some examples from example.lua.
 playereffects.use_examples = false
