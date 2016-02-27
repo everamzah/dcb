1db5c079fa268e603c2bd4925244f0f22d345c87
James Stevenson <everamzah@gmail.com>
2016-02-27 14:21:36 -0500

    Make backgrounds transparent

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -9,7 +9,7 @@ default = {}
 default.LIGHT_MAX = 14
 
 -- GUI related stuff
-default.gui_bg = "bgcolor[#080808BB;true]"
+default.gui_bg = "" --bgcolor[#080808BB;true]"
 default.gui_bg_img = "background[5,5;1,1;gui_formbg.png;true]"
 default.gui_slots = "listcolors[#00000069;#5A5A5A;#141318;#30434C;#FFF]"
 
