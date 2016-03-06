e5fa6531a0363c5d1a65d0a6fe8ae26810977af5
James Stevenson <everamzah@gmail.com>
2016-03-06 14:32:05 -0500

    Add recipes for crafting dirt_with_grass and dirt_with_dry_grass

--- mods/dcb/crafting.lua
+++ mods/dcb/crafting.lua
@@ -2,7 +2,20 @@ minetest.register_craftitem("dcb:fence_post", {
 	description = "Fence Post",
 	inventory_image = "dcb_fence_post.png",
 })
+
 minetest.register_craft({
 	output = "dcb:fence_post 2",
 	recipe = {{"group:stick", "group:stick", "group:stick"}, {"group:stick", "group:stick", "group:stick"}}
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:dirt_with_dry_grass",
+	recipe = {"default:dirt", "default:dry_grass_1"}
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "default:dirt_with_grass",
+	recipe = {"default:dirt", "default:grass_1"}
 })
