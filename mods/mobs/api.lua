e55c779dc8f9fe007e3c87f99b8492f3b10850cf
James Stevenson <everamzah@gmail.com>
2016-03-06 09:25:00 -0500

    Try mobs with catch_up false on ABMs

--- mods/mobs/api.lua
+++ mods/mobs/api.lua
@@ -2093,6 +2093,7 @@ function mobs:spawn_specific(name, nodes, neighbors, min_light, max_light,
 		neighbors = neighbors,
 		interval = interval,
 		chance = chance,
+		catch_up = false,
 
 		action = function(pos, node, _, active_object_count_wider)
 
