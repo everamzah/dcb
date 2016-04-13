f57af3b0f2c92b3af725e4b24a1643773b812083
James Stevenson <everamzah@gmail.com>
2016-04-13 08:54:42 -0400

    Initial update of Food Blocks\!

--- mods/foodblock/init.lua
+++ mods/foodblock/init.lua
@@ -1,21 +1,8 @@
--- Global foodblock namespace
--- foodblock = {}
--- foodblock.path = minetest.get_modpath("foodblock")
 local modpath = minetest.get_modpath("foodblock")
 
--- Load files
+dofile(modpath .. "/foodblock.lua")
+dofile(modpath .. "/mobsfood.lua")
+dofile(modpath .. "/crops.lua")
+dofile(modpath .. "/rice.lua")
 
--- Food support
-dofile(modpath.."/foodblock.lua")
-dofile(modpath.."/mobsfood.lua")
-dofile(modpath.."/crops.lua")
---dofile(modpath.."/farmplus.lua")
---dofile(modpath.."/plantlife.lua")
---dofile(modpath.."/foodfood.lua")
---dofile(modpath.."/moretree.lua")
---dofile(modpath.."/berrys.lua")
---dofile(modpath.."/ethereal.lua")
---dofile(modpath.."/farmredo.lua")
-
--- Cooking?
-dofile(modpath.."/cooking.lua")
+dofile(modpath .. "/cooking.lua")
