25585a5ec80c453e4a7919c25f1b32765a6e8d0e
James Stevenson <everamzah@gmail.com>
2016-04-26 05:04:27 -0400

    Re-add ceiling candle
    Try hand at nice selection box, close

--- mods/xdecor/nodes.lua
+++ mods/xdecor/nodes.lua
@@ -81,14 +81,14 @@ xdecor.register("candle", {
 	groups = {dig_immediate=3, attached_node=1},
 	tiles = {{name = "xdecor_candle_floor.png",
 			animation = {type="vertical_frames", length=1.5}},
-		{name = "xdecor_candle_floor.png",
+		{name = "xdecor_candle_ceiling.png",
 			animation = {type="vertical_frames", length=1.5}},
 		{name = "xdecor_candle_wall.png",
 			animation = {type="vertical_frames", length=1.5}}
 	},
 	selection_box = {
 		type = "wallmounted",
-		wall_top = {-0.25, -0.5, -0.25, 0.25, 0.1, 0.25},
+		wall_top = {-0.25, -0.35, -0.25, 0.25, 0.5, 0.25},
 		wall_bottom = {-0.25, -0.5, -0.25, 0.25, 0.1, 0.25},
 		wall_side = {-0.5, -0.35, -0.15, -0.15, 0.4, 0.15}
 	}
